b41a0be679506addcb43fd54ffbf0d1b7a401d1b
Kotaro Kirihara <88430903+KotaroKirihara07@users.noreply.github.com>
2025-11-18 23:09:59 +0900

    Add files via upload

--- terraform/aws/codebuild/main.tf
+++ terraform/aws/codebuild/main.tf
@@ -2,10 +2,6 @@ resource "aws_codebuild_project" "codebuild_project" {
   name          = "test-project"
   service_role  = aws_iam_role.example.arn
 
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-
   environment {
     type                        = "LINUX_CONTAINER"
     compute_type                = "BUILD_GENERAL1_SMALL"
