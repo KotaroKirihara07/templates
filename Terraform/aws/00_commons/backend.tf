d43a13a1a75a869c4ef1e3687d1c58ae529927d7
Kotaro Kirihara <88430903+KotaroKirihara07@users.noreply.github.com>
2026-01-31 23:50:34 +0900

    Add files via upload

--- Terraform/aws/00_commons/backend.tf
+++ Terraform/aws/00_commons/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "terraform-state-123456789012"
-        key = "states/backend.tfstate"
+        key = "resource_name/backend.tfstate"
         region = "ap-northeast-1"
         use_lockfile = true
     }
