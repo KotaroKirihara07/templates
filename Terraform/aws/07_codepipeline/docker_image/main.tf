198255f9500b4910036edbab14ada13d36c2352c
Kotaro Kirihara <88430903+KotaroKirihara07@users.noreply.github.com>
2026-02-11 23:39:34 +0900

    Add files via upload

--- Terraform/aws/07_codepipeline/docker_image/main.tf
+++ Terraform/aws/07_codepipeline/docker_image/main.tf
@@ -98,127 +98,75 @@ resource "aws_cloudwatch_log_group" "codebuild_project_log_group" {
   }
 }
 
-/*
+
 # CodePipline
 resource "aws_codepipeline" "codepipeline" {
-  name     = "tf-test-pipeline"
-  role_arn = aws_iam_role.codepipeline_role.arn
-
+  name           = "${var.prefix}_codepipeline"
+  execution_mode = "QUEUED"
+  pipeline_type  = "V2"
+  role_arn       = "arn:aws:iam::${data.aws_caller_identity.self.account_id}:role/service-role/AWSCodePipelineServiceRole-${data.aws_region.current.name}-${var.prefix}_codepipeline"
+  
   artifact_store {
-    location = aws_s3_bucket.codepipeline_bucket.bucket
+    location = "codepipeline-${data.aws_region.current.name}-${data.aws_caller_identity.self.account_id}"
+    region   = "${data.aws_region.current.name}"
     type     = "S3"
-
-    encryption_key {
-      id   = data.aws_kms_alias.s3kmskey.arn
-      type = "KMS"
-    }
   }
-
+  
   stage {
     name = "Source"
-
     action {
-      name             = "Source"
-      category         = "Source"
-      owner            = "AWS"
-      provider         = "CodeStarSourceConnection"
-      version          = "1"
-      output_artifacts = ["source_output"]
-
+      category = "Source"
       configuration = {
-        ConnectionArn    = aws_codestarconnections_connection.example.arn
-        FullRepositoryId = "my-organization/example"
-        BranchName       = "main"
+        BranchName           = "main"
+        OutputArtifactFormat = "CODE_ZIP"
+        PollForSourceChanges = "false"
+        RepositoryName       = "${var.prefix}_codecommit_repository"
+      }
+      name               = "Source"
+      namespace          = "SourceVariables"
+      output_artifacts   = ["SourceArtifact"]
+      owner              = "AWS"
+      provider           = "CodeCommit"
+      region             = "${data.aws_region.current.name}"
+      role_arn           = null
+      run_order          = 1
+      version            = "1"
+    }
+    on_failure {
+      result = "RETRY"
+      retry_configuration {
+        retry_mode = "ALL_ACTIONS"
       }
     }
   }
 
   stage {
     name = "Build"
-
     action {
-      name             = "Build"
-      category         = "Build"
-      owner            = "AWS"
-      provider         = "CodeBuild"
-      input_artifacts  = ["source_output"]
-      output_artifacts = ["build_output"]
-      version          = "1"
-
+      category = "Build"
       configuration = {
-        ProjectName = "test"
+        ProjectName = "${var.prefix}_codebuild_project"
+      }
+      input_artifacts    = ["SourceArtifact"]
+      name               = "Build"
+      namespace          = "BuildVariables"
+      output_artifacts   = ["BuildArtifact"]
+      owner              = "AWS"
+      provider           = "CodeBuild"
+      region             = "${data.aws_region.current.name}"
+      role_arn           = null
+      run_order          = 1
+      version            = "1"
+    }
+    on_failure {
+      result = "RETRY"
+      retry_configuration {
+        retry_mode = "ALL_ACTIONS"
       }
     }
   }
 
-  stage {
-    name = "Deploy"
-
-    action {
-      name            = "Deploy"
-      category        = "Deploy"
-      owner           = "AWS"
-      provider        = "CloudFormation"
-      input_artifacts = ["build_output"]
-      version         = "1"
-
-      configuration = {
-        ActionMode     = "REPLACE_ON_FAILURE"
-        Capabilities   = "CAPABILITY_AUTO_EXPAND,CAPABILITY_IAM"
-        OutputFileName = "CreateStackOutput.json"
-        StackName      = "MyStack"
-        TemplatePath   = "build_output::sam-templated.yaml"
-      }
-    }
+  tags = {
+    Name = "${var.prefix}_codepipeline"
   }
 }
-
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["codepipeline.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "codepipeline_role" {
-  name               = "test-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-data "aws_iam_policy_document" "codepipeline_policy" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "s3:GetObject",
-      "s3:GetObjectVersion",
-      "s3:GetBucketVersioning",
-      "s3:PutObjectAcl",
-      "s3:PutObject",
-    ]
-
-    resources = [
-      aws_s3_bucket.codepipeline_bucket.arn,
-      "${aws_s3_bucket.codepipeline_bucket.arn}/*"
-    ]
-  }
-
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "codebuild:BatchGetBuilds",
-      "codebuild:StartBuild",
-    ]
-
-    resources = ["*"]
-  }
-}
-*/
