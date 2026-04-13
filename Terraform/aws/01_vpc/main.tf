1843774b9c25bb060d64c7118a6b8629cab85e4d
Kotaro Kirihara <88430903+KotaroKirihara07@users.noreply.github.com>
2026-04-13 22:50:18 +0900

    Add files via upload

---
+++ Terraform/aws/01_vpc/main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  prefix = var.prefix
+  vpc_cidr_block = var.vpc_cidr_block
+  public_subnet_cidr_block = var.public_subnet_cidr_block
+  private_subnet_cidr_block = var.private_subnet_cidr_block
+}
